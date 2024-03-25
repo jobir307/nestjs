ab31899c9e577a24b3045a4191e40e81be2b28df
thebest05 <abduraximovazizjon610@gmail.com>
2024-03-25 17:11:09 +0300

    models

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
   const port = configService.get('port')
+  app.useGlobalPipes(new ValidationPipe())
+
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('NestJS lesson API')
+    .setDescription('This API for NestJS lesson')
+    .setVersion('7.3.0')
+    .addTag("API")
+    .build()
+
+  const document = SwaggerModule.createDocument(app, configSwagger)
+  SwaggerModule.setup('api', app, document)
   await app.listen(port)
 }
-bootstrap();
+bootstrap()
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
+import { UserController } from './controller/user.controller';
+import { UserService } from './service/user.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './model/user.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService]
 })
