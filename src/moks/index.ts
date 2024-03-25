200059715f1a5d24c8f503d404d98264dc760467
thebest05 <abduraximovazizjon610@gmail.com>
2024-03-25 11:58:39 +0300

    done

---
+++ src/moks/index.ts
@@ -0,0 +1,317 @@
+export const users = [
+    {
+      "_id": "65fec54e8fe3242639ae4bd8",
+      "index": 0,
+      "guid": "197a0829-a1b4-4ddf-a8a3-319f64aeac37",
+      "isActive": true,
+      "balance": "$1,565.39",
+      "picture": "http://placehold.it/32x32",
+      "age": 21,
+      "eyeColor": "blue",
+      "name": "Lauri Tran",
+      "gender": "female",
+      "company": "FISHLAND",
+      "email": "lauritran@fishland.com",
+      "phone": "+1 (949) 578-2590",
+      "address": "599 Jackson Place, Efland, Nebraska, 2005",
+      "about": "Eu qui ex laboris cupidatat labore. Occaecat commodo velit id est laboris eu culpa exercitation aute irure nisi elit amet voluptate. Eiusmod reprehenderit quis esse proident ea eiusmod aliqua ullamco aliqua ut. Irure laborum id nulla laboris.\r\n",
+      "registered": "2019-12-18T11:41:44 -03:00",
+      "latitude": -89.6408,
+      "longitude": 147.651121,
+      "tags": [
+        "commodo",
+        "sunt",
+        "in",
+        "nisi",
+        "duis",
+        "sint",
+        "sint"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Pace Cardenas"
+        },
+        {
+          "id": 1,
+          "name": "Blanchard Greene"
+        },
+        {
+          "id": 2,
+          "name": "Charles Castro"
+        }
+      ],
+      "greeting": "Hello, Lauri Tran! You have 2 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "65fec54e1042cb88cff112e8",
+      "index": 1,
+      "guid": "70a74d84-4a12-46d9-8d97-6407cc7b6891",
+      "isActive": false,
+      "balance": "$3,048.80",
+      "picture": "http://placehold.it/32x32",
+      "age": 29,
+      "eyeColor": "green",
+      "name": "Ann Foster",
+      "gender": "female",
+      "company": "SURELOGIC",
+      "email": "annfoster@surelogic.com",
+      "phone": "+1 (959) 571-3743",
+      "address": "578 Exeter Street, Waikele, Arizona, 1389",
+      "about": "Ullamco ea labore adipisicing voluptate. Anim laboris duis ex officia id et sunt ex labore sit tempor commodo consectetur. Mollit do sit aliqua sint laboris irure commodo ipsum deserunt reprehenderit pariatur quis tempor. Reprehenderit proident do deserunt labore pariatur cillum voluptate. Culpa quis dolor ad enim do nulla in est anim fugiat eiusmod consectetur nostrud excepteur. Irure sunt eu et adipisicing. Pariatur labore voluptate adipisicing non.\r\n",
+      "registered": "2021-04-12T02:36:39 -03:00",
+      "latitude": -73.234885,
+      "longitude": 85.520458,
+      "tags": [
+        "adipisicing",
+        "nostrud",
+        "ea",
+        "laborum",
+        "irure",
+        "cillum",
+        "eiusmod"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Lee Good"
+        },
+        {
+          "id": 1,
+          "name": "Kirsten Wilder"
+        },
+        {
+          "id": 2,
+          "name": "Dale Bowen"
+        }
+      ],
+      "greeting": "Hello, Ann Foster! You have 7 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "65fec54ee80232ed116f9286",
+      "index": 2,
+      "guid": "4e00d663-439e-4532-ad30-6bc7a8cc0f13",
+      "isActive": false,
+      "balance": "$3,648.76",
+      "picture": "http://placehold.it/32x32",
+      "age": 32,
+      "eyeColor": "blue",
+      "name": "Darcy Oneill",
+      "gender": "female",
+      "company": "SULTRAXIN",
+      "email": "darcyoneill@sultraxin.com",
+      "phone": "+1 (883) 426-3123",
+      "address": "450 Boerum Street, Fairmount, Tennessee, 5594",
+      "about": "Occaecat culpa deserunt et exercitation nostrud excepteur sint ullamco. Deserunt magna occaecat minim in tempor eiusmod. Occaecat voluptate ex aliquip occaecat culpa eu labore magna cillum magna dolore. Dolor exercitation Lorem esse commodo commodo adipisicing voluptate ad Lorem do.\r\n",
+      "registered": "2017-06-09T12:47:59 -03:00",
+      "latitude": -29.034801,
+      "longitude": 151.818778,
+      "tags": [
+        "excepteur",
+        "proident",
+        "magna",
+        "voluptate",
+        "do",
+        "irure",
+        "tempor"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Keisha Wood"
+        },
+        {
+          "id": 1,
+          "name": "Gracie Sloan"
+        },
+        {
+          "id": 2,
+          "name": "Carmella Riggs"
+        }
+      ],
+      "greeting": "Hello, Darcy Oneill! You have 9 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "65fec54e710c9bd3bd9a5ae4",
+      "index": 3,
+      "guid": "59af73d7-77c9-4066-8b39-54717c8eab26",
+      "isActive": false,
+      "balance": "$1,430.26",
+      "picture": "http://placehold.it/32x32",
+      "age": 37,
+      "eyeColor": "brown",
+      "name": "Griffin Flynn",
+      "gender": "male",
+      "company": "HANDSHAKE",
+      "email": "griffinflynn@handshake.com",
+      "phone": "+1 (977) 597-2151",
+      "address": "915 Willow Street, Talpa, South Carolina, 5422",
+      "about": "Proident irure ullamco incididunt laborum est ut nostrud fugiat et veniam officia amet. Quis consequat minim pariatur aliquip elit ex nulla irure nostrud nulla do ex exercitation. Nisi velit minim commodo do veniam consequat culpa pariatur laborum ut do ut. Aliqua elit dolor sit amet. Proident tempor fugiat adipisicing ea nostrud Lorem. Sit aliqua non nulla eu tempor esse esse dolore do cupidatat.\r\n",
+      "registered": "2015-11-06T09:37:40 -03:00",
+      "latitude": 65.739715,
+      "longitude": -59.623162,
+      "tags": [
+        "commodo",
+        "sit",
+        "sint",
+        "adipisicing",
+        "culpa",
+        "mollit",
+        "ipsum"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Hollie Graves"
+        },
+        {
+          "id": 1,
+          "name": "Noble Hansen"
+        },
+        {
+          "id": 2,
+          "name": "Isabel Hamilton"
+        }
+      ],
+      "greeting": "Hello, Griffin Flynn! You have 6 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "65fec54e5ec1d829eefccedc",
+      "index": 4,
+      "guid": "54123ca6-1f8e-458d-898e-c5670acdd24b",
+      "isActive": false,
+      "balance": "$3,006.16",
+      "picture": "http://placehold.it/32x32",
+      "age": 40,
+      "eyeColor": "green",
+      "name": "Vickie Daugherty",
+      "gender": "female",
+      "company": "REALYSIS",
+      "email": "vickiedaugherty@realysis.com",
+      "phone": "+1 (927) 430-2014",
+      "address": "301 Evans Street, Stouchsburg, California, 163",
+      "about": "Anim velit voluptate laborum minim quis sint exercitation officia sunt. Laborum voluptate excepteur est adipisicing duis eiusmod non ex. Elit cupidatat sit laborum ullamco magna amet. Esse officia eiusmod ex minim elit officia irure in id proident fugiat reprehenderit. Proident commodo nulla aliquip nostrud velit laborum pariatur commodo est id est esse. Qui eu duis ut exercitation nostrud elit nulla cillum pariatur.\r\n",
+      "registered": "2023-02-27T03:13:53 -03:00",
+      "latitude": -22.760766,
+      "longitude": 109.584137,
+      "tags": [
+        "pariatur",
+        "mollit",
+        "tempor",
+        "in",
+        "cupidatat",
+        "consectetur",
+        "cillum"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Becker Crane"
+        },
+        {
+          "id": 1,
+          "name": "Frost Cherry"
+        },
+        {
+          "id": 2,
+          "name": "Justice Fuentes"
+        }
+      ],
+      "greeting": "Hello, Vickie Daugherty! You have 10 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "65fec54e4c15a8a37210098a",
+      "index": 5,
+      "guid": "657e0af7-2beb-45e6-b1d1-873a0d73a531",
+      "isActive": false,
+      "balance": "$3,921.66",
+      "picture": "http://placehold.it/32x32",
+      "age": 40,
+      "eyeColor": "brown",
+      "name": "Stark Mann",
+      "gender": "male",
+      "company": "SEQUITUR",
+      "email": "starkmann@sequitur.com",
+      "phone": "+1 (848) 577-2786",
+      "address": "876 Menahan Street, Homestead, Oklahoma, 3834",
+      "about": "Duis quis incididunt magna exercitation. Dolor sint quis sint velit excepteur ut. Adipisicing duis enim aute laboris sint eu quis occaecat enim. Quis sit aute deserunt est consequat eiusmod cupidatat tempor elit. Dolore officia irure quis culpa non id anim nostrud cupidatat minim non do magna. Commodo magna nulla cillum proident.\r\n",
+      "registered": "2022-08-06T02:01:04 -03:00",
+      "latitude": 72.046773,
+      "longitude": -44.447152,
+      "tags": [
+        "eu",
+        "do",
+        "ea",
+        "aliqua",
+        "amet",
+        "ut",
+        "cupidatat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Mai Floyd"
+        },
+        {
+          "id": 1,
+          "name": "Fernandez Goodman"
+        },
+        {
+          "id": 2,
+          "name": "Heath Wheeler"
+        }
+      ],
+      "greeting": "Hello, Stark Mann! You have 3 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "65fec54e88d6143f7a72908d",
+      "index": 6,
+      "guid": "0c23ce58-d566-4246-87d5-3dc6747b3370",
+      "isActive": true,
+      "balance": "$3,849.49",
+      "picture": "http://placehold.it/32x32",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Ross Bird",
+      "gender": "male",
+      "company": "PYRAMIA",
+      "email": "rossbird@pyramia.com",
+      "phone": "+1 (918) 546-3159",
+      "address": "974 Bank Street, Robinette, Kentucky, 9473",
+      "about": "Ut cupidatat nisi dolor exercitation magna aute cillum ea laborum in non sint ex exercitation. Veniam officia excepteur eu officia incididunt irure aute occaecat duis. Culpa culpa veniam ea commodo laboris.\r\n",
+      "registered": "2018-04-26T11:48:04 -03:00",
+      "latitude": 88.672179,
+      "longitude": 41.615055,
+      "tags": [
+        "eu",
+        "elit",
+        "velit",
+        "aliqua",
+        "nostrud",
+        "exercitation",
+        "culpa"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Roslyn Woodard"
+        },
+        {
+          "id": 1,
+          "name": "Wendy Mckinney"
+        },
+        {
+          "id": 2,
+          "name": "Lowe Mcgowan"
+        }
+      ],
+      "greeting": "Hello, Ross Bird! You have 2 unread messages.",
+      "favoriteFruit": "banana"
+    }
+]
